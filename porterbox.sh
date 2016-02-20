e6b815f55d8a5bc1ec6108ef11e230e2c59dc375
HAYASHI Kentaro <kenhys@gmail.com>
2016-02-21 01:07:01 +0900

    Add comment

--- porterbox.sh
+++ porterbox.sh
@@ -6,10 +6,12 @@ install_packages() {
     dd-schroot-cmd -c $SCHROOTID apt-get update
     dd-schroot-cmd -c $SCHROOTID apt-get -y upgrade
     dd-schroot-cmd -c $SCHROOTID apt-get -y build-dep groonga
-    dd-schroot-cmd -c $SCHROOTID apt-get -y install procps packaging-dev git dh-autoreconf ca-certificates libssl-dev libreadline-dev zlib1g-dev
-
+    dd-schroot-cmd -c $SCHROOTID apt-get -y install procps packaging-dev git dh-autoreconf ca-certificates
+    # for ruby
+    dd-schroot-cmd -c $SCHROOTID apt-get -y install libssl-dev libreadline-dev zlib1g-dev
+    # for groonga
     dd-schroot-cmd -c $SCHROOTID apt-get -y install liblz4-dev zlib1g-dev libmsgpack-dev libzmq3-dev libevent-dev libmecab-dev libpcre3-dev libstemmer-dev libjemalloc-dev
-
+    dd-schroot-cmd -c $SCHROOTID apt-get -y install mecab-naist-jdic
 }
 
 SCHROOTID=
