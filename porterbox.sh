e9a684d0de61033e1bdb773c541f35a6b85f2140
HAYASHI Kentaro <kenhys@gmail.com>
2016-02-20 22:46:12 +0900

    Install packages

--- porterbox.sh
+++ porterbox.sh
@@ -2,20 +2,30 @@
 
 set -x
 
+install_packages() {
+    dd-schroot-cmd -c $SCHROOTID apt-get update
+    dd-schroot-cmd -c $SCHROOTID apt-get -y upgrade
+    dd-schroot-cmd -c $SCHROOTID apt-get -y build-dep groonga
+    dd-schroot-cmd -c $SCHROOTID apt-get -y install procps packaging-dev git dh-autoreconf
+}
+
 SCHROOTID=
 
 case $1 in
     sid)
 	SCHROOTID=sid$RANDOM
 	schroot -b -c sid -n $SCHROOTID
+	install_packages
 	;;
     jessie)
 	SCHROOTID=jessie$RANDOM
 	schroot -b -c jessie -n $SCHROOTID
+	install_packages
 	;;
     wheezy)
 	SCHROOTID=wheezy$RANDOM
 	schroot -b -c wheezy -n $SCHROOTID
+	install_packages
 	;;
     ruby)
 	if [ $HOME = "/home/kenhys" ]; then
@@ -30,7 +40,3 @@ case $1 in
 	;;
 esac
 
-dd-schroot-cmd -c $SCHROOTID apt-get update
-dd-schroot-cmd -c $SCHROOTID apt-get -y upgrade
-dd-schroot-cmd -c $SCHROOTID apt-get -y build-dep groonga
-dd-schroot-cmd -c $SCHROOTID apt-get -y install procps packaging-dev git dh-autoreconf
